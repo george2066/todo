b5c63aa43b3ab49eaaee8bcbe60aca173e9b1416
lomalo890 <george2066@yandex.ru>
2025-02-19 22:00:10 +0300

    add: visualized for user with todo_item and todo_list

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,26 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from tasks.models import TodoList
+from tasks.models import TodoList, TodoItem
 from django.urls import reverse_lazy
 
 # Create your views here.
 class TodoListListView(LoginRequiredMixin, ListView):
 
-    login = reverse_lazy('account_login')
+    login_url = reverse_lazy('account_login')
     template_name = 'tasks/index.html'
 
     def get_queryset(self):
         return TodoList.objects.for_user(self.request.user)
+
+class TodoItemListView(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('account_login')
+    template_name = 'tasks/todo_list.html'
+
+    def get_queryset(self):
+        todo_list = TodoList.objects.for_user(self.request.user).filter(pk=self.kwargs['list_id'])
+        if todo_list is None:
+            raise PermissionDenied()
+        return TodoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
+
