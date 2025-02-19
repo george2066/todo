a9a9eef020deac68e83635e087e2d3a9a944d8fc
lomalo890 <george2066@yandex.ru>
2025-02-19 20:00:21 +0300

    add: classes TodoLists

---
+++ tasks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from tasks.views import TodoListListView
+
+urlpatterns = [
+    path("", TodoListListView.as_view(), name='list')
+]
--- todo/urls.py
+++ todo/urls.py
@@ -11,8 +11,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
 
 from django.contrib import admin
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path("", include('tasks.urls')),
 ]
